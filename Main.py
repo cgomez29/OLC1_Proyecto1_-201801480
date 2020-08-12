e8a6bbe83a2b3e035076b2da258c317d66faf682
Cristian Gomez <2912196670108@ingenieria.usac.edu.gt>
2020-08-12 15:21:59 -0600

    Inicio, Main, MainWindow

---
+++ Main.py
@@ -0,0 +1,10 @@
+
+from ui.MainWindow import MainWindow
+
+class Main:
+    
+    def __init__(self):
+        self.start = MainWindow()
+        self.start.showWindow()
+
+main = Main()
---
+++ ui/MainWindow.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+class MainWindow:
+
+    def __init__(self):
+        self.window = tk.Tk()
+
+    
+    def showWindow(self):
+        self.window.mainloop()
+
+
+    def guardarComo():
+        pass
+
+    def salir():
+        pass
+
